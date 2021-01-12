7d76e1355fa27e268f2a8a2baab1dda68ae2aa7d
Arvind <arvind.kumar@impressico.com>
2021-01-12 17:20:30 +0530

    Initial Commit

---
+++ SwiftUICustomPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomPlayer
+//
+//  Created by Arvind on 12/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
